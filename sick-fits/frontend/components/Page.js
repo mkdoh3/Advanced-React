9a2ccfba7f29198ca2c64b5f6362ef30575f826f
mkdoh3 <mkdoherty3@gmail.com>
2022-08-24 13:19:32 -0500

    feat(app): implements nprogress progress bar and styles

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Header from "./Header";
 
 const GlobalStyles = createGlobalStyle`
@@ -42,13 +42,18 @@ const GlobalStyles = createGlobalStyle`
 
 `;
 
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 export default function Page({ children }) {
   return (
     <div>
       <GlobalStyles />
       <Header />
-      <h1>Page component</h1>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
